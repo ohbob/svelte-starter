ff5a5690096be274d1024861ea0f325796ab3a2e
Nathaniel Tampus <nate@mugnavo.com>
2025-02-24 11:12:47 +0800

    update auth config

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -6,12 +6,20 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
 
 export const auth = betterAuth({
+	baseURL: PUBLIC_BASE_URL,
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
-	// emailAndPassword: {
-	// 	enabled: true,
-	// },
+
+	// https://www.better-auth.com/docs/concepts/session-management#session-caching
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: 5 * 60, // 5 minutes
+		},
+	},
+
+	// https://www.better-auth.com/docs/concepts/oauth
 	socialProviders: {
 		github: {
 			clientId: env.GITHUB_CLIENT_ID!,
@@ -26,5 +34,9 @@ export const auth = betterAuth({
 			clientSecret: env.DISCORD_CLIENT_SECRET!,
 		},
 	},
-	baseURL: PUBLIC_BASE_URL,
+
+	// https://www.better-auth.com/docs/authentication/email-password
+	// emailAndPassword: {
+	//   enabled: true,
+	// },
 });
